88be3a0a7339188f412771e0d7034527b2d45a96
andrestraspuesto <andrestraspuesto@gmail.com>
2017-11-24 14:19:11 +0100

    add test

---
+++ library-monolith/src/main/java/es/codemotion/madrid/library/repositories/BookRepositoryTest.java
@@ -0,0 +1,64 @@
+package es.codemotion.madrid.library.repositories;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BookRepositoryTest {
+
+	@Test
+	public void testSaveS() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSaveIterableOfS() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindOne() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExists() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindAllIterableOfID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteT() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteIterableOfQextendsT() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteAll() {
+		fail("Not yet implemented");
+	}
+
+}
